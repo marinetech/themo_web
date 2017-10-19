c3296028ccf7deddc5e9d6c37a9db22a8dc991e7
Ilan <ilan.mardix@gmail.com>
2017-10-19 10:37:02 +0300

    added units added additional s9 sensors added valign to table

--- utils/edit_sensor_description.js
+++ utils/edit_sensor_description.js
@@ -11,59 +11,59 @@ var dcs_fields = [dcs_display_name, dsc_description, dsc_model];
 sensors["dcs"] = dcs_fields
 
 var flntu_display_name = "Fluorescence"
-var flntu_description = "Fluorometer - Chlorophyll(mg/L) & Turbidity"
+var flntu_description = "Fluorometer - Chlorophyll(μg/L) & Turbidity(NTU)"
 flntu_description += "\n(depth: 1m)"
 var flntu_model = "Wet Labs - ECOFLNTUS"
 var flntu_fields = [flntu_display_name, flntu_description, flntu_model]
 sensors["flntu"] = flntu_fields
 
 var microcat_display_name = "Salinity"
-var microcat_description = "Measures salinity conductivity & temprature"
+var microcat_description = "Measures salinity(PSU), conductivity(S/m) & temprature(C)"
 microcat_description += "\n(depth: 1m)"
 var microcat_model = "Seabird Electronics	- SBE37-SI [microcat]"
 var microcat_fields = [microcat_display_name, microcat_description, microcat_model]
 sensors["microcat"] = microcat_fields
 
 var s9_display_name = "Sea temprature"
-var s9_description = "Measures temprature in different depths - "
+var s9_description = "Measures temprature(C) in different depths - "
 s9_description += "\n(5m - 85m in steps of 10m)"
 var s9_model = "Sound-nine Ulti-Modem MCIL4M"
 var s9_fields = [s9_display_name, s9_description, s9_model]
 sensors["s9"] = s9_fields
 
 var metpak_display_name = "Weather Station"
-var metpak_description = "External temprature & humidity, wind velocity, pressure"
+var metpak_description = "External temprature(C) & humidity(RH%), wind velocity(Metres/second), pressure(millibars)"
 metpak_description += "\n(installed 3m above sea)"
 var metpak_model = "Gill - MetPak"
 var metpak_fields = [metpak_display_name, metpak_description, metpak_model]
 sensors["metpak"] = metpak_fields
 
 var windsonic_display_name = "Anemometer"
-var windsonic_description = "Measures wind velocity.\n(installed 3m above sea)"
+var windsonic_description = "Measures wind velocity (Metres/second) - installed 3m above sea"
 var windsonic_model = "Gill - WindSonic"
 var windsonic_fields = [windsonic_display_name,windsonic_description,windsonic_model]
 sensors["windsonic"] = windsonic_fields
 
 var humidity_display_name = "humidity"
-var humidity_description = "External humidity. (installed 3m above sea)"
+var humidity_description = "External humidity(RH%) - installed 3m above sea"
 var humidity_model = "Rotronics - mp101a"
 var mp101a_humidity_fields = [humidity_display_name,humidity_description,humidity_model]
 sensors["mp101a_humidity"] = mp101a_humidity_fields
 
 var temprature_display_name = "Temprature"
-var temprature_description = "External temprature. (installed 3m above sea)"
+var temprature_description = "External temprature(C) - installed 3m above sea"
 var temprature_model = "Rotronics - mp101a"
 var mp101a_temprature_fields = [temprature_display_name, temprature_description, temprature_model]
 sensors["mp101a_temprature"] = mp101a_temprature_fields
 
 var barometer_display_name = "Barometer"
-var barometer_description = "Barometric pressure. (installed 3m above sea)"
+var barometer_description = "Barometric pressure(millibars) - installed 3m above sea"
 var barometer_model = "Vaisala - PTB210"
 var barometer_fields = [barometer_display_name,barometer_description,barometer_model]
 sensors["barometer"] = barometer_fields
 
 var waves_display_name = "Sea waves"
-var waves_description = "waves higth"
+var waves_description = "waves higth(Meters)"
 var waves_model = "Lord MicroStrain	- 3DM-GX25"
 var waves_fields = [waves_display_name,waves_description,waves_model]
 sensors["waves"] = waves_fields
---
+++ utils/set_s9_sensors_list.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+var async = require('async')
+
+
+var s9_list = [ {'A016': 5}, {'A00B': 14}, {'A015': 23}, {'A00H': 32},
+                            {'A00W': 41}, {'A00D': 50}, {'A00A': 59}, {'A00E': 68}, {'A00C': 77}, {'A00T': 86}, {'A017': 5}, {'A00S': 14}, {'A018': 23}, {'A00Y': 32}];
+
+
+mongoose.Promise = global.Promise; // solution for mongoose Promise warrning
+var strConnection = 'mongodb://127.0.0.1/themo';
+console.log("connecting to themo db...");
+mongoose.connect(strConnection);
+var db = mongoose.connection;
+
+db.once('open', function() {
+
+
+    var sensor_model = require('../app/models/sensor');
+    sensor_model.findOne({'name': 's9', 'buoy_name': 'tabs225m09'}, function(err, sens){
+       sens.update({"child_sensors" : s9_list}, function() {
+         console.log("-I- done updating s9_list");
+         mongoose.connection.close();
+       })
+    });
+
+
+}); // db.once
