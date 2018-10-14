b7ddf979f5bd4c9257f100baa32132006a041676
Ilan Mardix <ilan.mardix@gmail.com>
2018-10-14 09:31:48 +0300

    ais status was added

--- app/controllers/status.js
+++ app/controllers/status.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var Sample = require('../models/sample');
+var scrape = require('../lib/scrape');
 var async = require('async');
+const rp = require('request-promise');
+const cheerio = require('cheerio');
 
 var report = {};
 
@@ -12,7 +15,16 @@ router.get('/', function (req, res) {
 	arr_funcs.push(get_last_communication_time);
 	arr_funcs.push(get_last_battery_docs);
 	arr_funcs.push(get_last_s9);
-
+	arr_funcs.push(get_last_microcat);
+	arr_funcs.push(get_last_windsonic);
+	arr_funcs.push(get_last_metpak);
+	arr_funcs.push(get_last_adcp);
+	arr_funcs.push(get_last_dcs);
+	arr_funcs.push(get_last_flntu);
+	arr_funcs.push(get_last_microstrain);
+	arr_funcs.push(get_last_humidity);
+	arr_funcs.push(get_last_external_temperature);
+	arr_funcs.push(get_last_ais);
 	async.parallel(arr_funcs, function(err) {
 		console.log(report);
 		res.render('status', {report: report} )
@@ -27,7 +39,7 @@ function get_last_communication_time(callback) {
 	console.log("-I- running get_last_communication_time");
 	Sample.findOne({}, {}, { sort: { 'd_stamp' : -1,  't_stamp' : -1} }, function(err, doc) {
 		iso_date =  extract_ISO_date(doc)
-		report.lastcomm = iso_date.toUTCString();
+		report.lastcomm = iso_date.toString();
 		callback()
 	});
 }
@@ -41,7 +53,7 @@ function get_last_battery_docs(callback) {
 							bid = "battery" + b_id;
 							level = doc._doc.level;
 							// console.log("level: " + level);
-							report[bid] = {level : level, date: iso_date.toUTCString()};
+							report[bid] = {level : level, date: iso_date.toString()};
 							cb();
 						})
 
@@ -72,7 +84,7 @@ function get_last_s9(callback) {
 			s9_report.num_of_sensors = docs.length;
 			var arr_sensors = [];
 			for (var i = 0; i < docs.length; i++) {
-				sensor_info = {s9_id : docs[i].s9_id, temp : docs[i].temperature, depth : docs[i].depth};
+				sensor_info = {s9_id : docs[i]._doc.s9_id, temp : docs[i]._doc.temperature, depth : docs[i]._doc.depth};
 				arr_sensors.push(sensor_info);
 			}
 			s9_report.sensors = arr_sensors;
@@ -81,7 +93,131 @@ function get_last_s9(callback) {
 		})
 })}
 
+function get_last_microcat(callback) {
+	console.log("-I- running get_last_microcat");
+	Sample.findOne({'sensor_name': 'microcat'}, {}, { sort: { 'd_stamp' : -1,  't_stamp' : -1} }, function(err, doc) {
+		var microcat = {};
+		microcat.iso_date =  extract_ISO_date(doc).toString();
+		microcat.salinity =  doc._doc.Salinity;
+		microcat.conductivity =  doc._doc.Conductivity;
+		microcat.temperature =  doc._doc.Temperature;
+		report.microcat = microcat;
+		callback()
+	});
+}
 
+function get_last_windsonic(callback) {
+	console.log("-I- running get_last_windsonic");
+	Sample.findOne({'sensor_name': 'windsonic'}, {}, { sort: { 'd_stamp' : -1,  't_stamp' : -1} }, function(err, doc) {
+		var windsonic = {};
+		windsonic.iso_date =  extract_ISO_date(doc).toString();
+		windsonic.winddirection =  doc._doc.winddirection;
+		windsonic.magnitude =  doc._doc.magnitude_1;
+		report.windsonic = windsonic;
+		callback()
+	});
+}
+
+function get_last_metpak(callback) {
+	console.log("-I- running metpak");
+	Sample.findOne({'sensor_name': 'metpak'}, {}, { sort: { 'd_stamp' : -1,  't_stamp' : -1} }, function(err, doc) {
+		var metpak = {};
+		metpak.iso_date =  extract_ISO_date(doc).toString();
+		metpak.winddirection =  doc._doc.wind_direction;
+		metpak.speed =  doc._doc.wind_speed;
+		metpak.temperature =  doc._doc.temperature;
+		metpak.humidity =  doc._doc.humidity;
+		report.metpak = metpak;
+		callback()
+	});
+}
+
+function get_last_adcp(callback) {
+	console.log("-I- running adcp");
+	Sample.findOne({'sensor_name': 'adcp'}, {}, { sort: { 'd_stamp' : -1,  't_stamp' : -1} }, function(err, doc) {
+		var adcp = {};
+		adcp.iso_date =  extract_ISO_date(doc).toString();
+		report.adcp = adcp;
+		callback()
+	});
+}
+
+function get_last_dcs(callback) {
+	console.log("-I- running dcs");
+	Sample.findOne({'sensor_name': 'dcs'}, {}, { sort: { 'd_stamp' : -1,  't_stamp' : -1} }, function(err, doc) {
+		var dcs = {};
+		dcs.iso_date =  extract_ISO_date(doc).toString();
+		report.dcs = dcs;
+		callback()
+	});
+}
+
+function get_last_flntu(callback) {
+	console.log("-I- running flntu");
+	Sample.findOne({'sensor_name': 'flntu'}, {}, { sort: { 'd_stamp' : -1,  't_stamp' : -1} }, function(err, doc) {
+		var flntu = {};
+		flntu.iso_date =  extract_ISO_date(doc).toString();
+		report.flntu = flntu;
+		callback()
+	});
+}
+
+function get_last_microstrain(callback) {
+	console.log("-I- running microtrain");
+		Sample.findOne({'sensor_name': 'waves'}, {}, { sort: { 'd_stamp' : -1,  't_stamp' : -1} }, function(err, doc) {
+			var waves = {};
+			waves.iso_date =  extract_ISO_date(doc).toString();
+			waves.mean_period =  doc._doc.mean_period;
+			waves.dominant_period =  doc._doc.dominant_period;
+			waves.significant_height =  doc._doc.significant_height;
+			waves.iso_date =  extract_ISO_date(doc).toString();
+			report.waves = waves;
+			callback()
+	});
+}
+
+function get_last_humidity(callback) {
+	console.log("-I- running humidity");
+		Sample.findOne({'sensor_name': 'mp101a_humidity'}, {}, { sort: { 'd_stamp' : -1,  't_stamp' : -1} }, function(err, doc) {
+			var humidity = {};
+			humidity.iso_date =  extract_ISO_date(doc).toString();
+			humidity.humidity =  doc._doc.AvgLinearAdjVal;
+			report.humidity = humidity;
+			callback()
+	});
+}
+
+function get_last_external_temperature(callback) {
+	Sample.findOne({'sensor_name': 'mp101a_temprature'}, {}, { sort: { 'd_stamp' : -1,  't_stamp' : -1} }, function(err, doc) {
+		var temperature = {};
+		temperature.iso_date =  extract_ISO_date(doc).toString();
+		temperature.temperature =  doc._doc.AvgLinearAdjVal;
+		report.temperature = temperature;
+		callback()
+	});
+}
+
+function get_last_ais(callback) {
+	console.log("-I- running last_ais");
+
+	var options = {
+	  uri: `https://www.marinetraffic.com/en/ais/details/ships/shipid:5689729/mmsi:244010235/vessel:THEMO%20BOUY`,
+	  transform: function (body) {
+	    return cheerio.load(body);
+	  }
+	};
+
+	rp(options)
+    .then(($) => {
+      $('.group-ib').each(function(i, elem) {
+        if ($(this).text().indexOf('Position Received:') >= 0) {
+            report.ais = $(this).text().replace(/ +(?= )/g,'').trim().replace("\n", "");
+          }
+      });
+			callback();
+    }
+
+	)}
 
 function extract_ISO_date(doc) {
 	date_str =  doc._doc.d_stamp + 'T' + doc._doc.t_stamp + 'Z';
